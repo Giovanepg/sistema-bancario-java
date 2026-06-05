a4d69ea0fbe02fee9a222b37ef90728236efe2f4
Giovane Ferreira <ferreirapaesribeirog@gmail.com>
2026-06-05 20:56:47 -0300

    Adiciona interface Bloqueavel

---
+++ src/main/java/aprendendojava/sistema/bancario/Bloqueavel.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aprendendojava.sistema.bancario;
+
+/**
+ *
+ * @author Anônimo
+ */
+public interface Bloqueavel {
+
+    void bloquear();
+
+    void desbloquear();
+
+    boolean isAtiva();
+}
