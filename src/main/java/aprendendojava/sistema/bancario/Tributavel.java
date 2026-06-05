306304f32af52b2ac2f0aec0e93e5b5402773b4d
Giovane Ferreira <ferreirapaesribeirog@gmail.com>
2026-06-05 20:57:30 -0300

    Adiciona interface Tributavel

---
+++ src/main/java/aprendendojava/sistema/bancario/Tributavel.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aprendendojava.sistema.bancario;
+
+/**
+ *
+ * @author Anônimo
+ */
+public interface Tributavel {
+
+    double calcularImposto();
+}
