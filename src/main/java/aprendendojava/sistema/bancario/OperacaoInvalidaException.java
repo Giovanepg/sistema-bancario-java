14a29009ea65c73649a6b414f3182c7fb0e9a7f5
Giovane Ferreira <ferreirapaesribeirog@gmail.com>
2026-06-05 20:58:45 -0300

    Adiciona exceção OperacaoInvalidaException

---
+++ src/main/java/aprendendojava/sistema/bancario/OperacaoInvalidaException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aprendendojava.sistema.bancario;
+
+/**
+ *
+ * @author Anônimo
+ */
+public class OperacaoInvalidaException extends Exception {
+
+    public OperacaoInvalidaException(String mensagem) {
+        super(mensagem);
+    }
+}
